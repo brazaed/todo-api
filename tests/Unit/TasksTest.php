f9779f6b6da886c0a073067342a43a773219ffa1
marcmes <mmesqdev@gmail.com>
2019-01-08 22:54:34 -0600

    add - taks test

---
+++ tests/Unit/TasksTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TasksTest extends TestCase
+{
+
+    use WithFaker, RefreshDatabase;
+
+
+    /** @test */
+    public function a_user_can_create_a_task()
+    {
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+
+            'title' => $this->faker->sentence,            
+            'description' => $this->faker->paragraph
+        ];
+        
+
+        $this->post('/api/tasks', $attributes);
+
+        $this->assertDatabaseHas('tasks', $attributes);
+        
+    }    
+}
